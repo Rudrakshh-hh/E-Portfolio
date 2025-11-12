74e300cf8797755643224a17d780f24d5f860eec
Rudraksh Bhatt <rudraaksh.bhatt@gmail.com>
2025-11-13 00:02:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '/E-Portfolio/', // 👈 add this line
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
